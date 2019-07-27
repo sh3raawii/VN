ac57d0064af2b110eb8113b6f4943b972c138837
Mostafa Mahmoud <mostafa_mahmoud@protonmail.com>
2019-07-27 05:41:07 +0200

    make log level configurable

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 const PORT = process.env.PORT || 80
 const NODE_ENV = process.env.NODE_ENV || 'development'
+const LOG_LEVEL = process.env.LOG_LEVEL || 'info'
 
 module.exports = {
     port: PORT,
-    environment: NODE_ENV
+    environment: NODE_ENV,
+    logLevel: LOG_LEVEL
 }
